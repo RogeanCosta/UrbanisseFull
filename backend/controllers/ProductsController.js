c09c65f6f0e43c8602e9e5fcb67b8d8e36c73308
ValnicioJunior <valniciogomessilvajunior10@gmail.com>
2025-09-03 11:28:43 -0300

    Adicionando novas rotas para produtos e usuários

--- backend/controllers/ProductsController.js
+++ backend/controllers/ProductsController.js
@@ -17,3 +17,49 @@ exports.deleteProduct = async (req, res) => {
     res.status(500).json({ error: 'Erro ao deletar produto' });
   }
 };
+
+exports.createProduct = async (req, res) => {
+  try {
+    const { name, price, description, stock, category, gender, imageUrl, imagePath } = req.body;
+    const newProduct = await prisma.products.create({
+      data: {
+        name,
+        price,
+        description,
+        stock,
+        category,
+        gender,
+        imageUrl,
+        imagePath,
+      },
+    });
+    res.status(201).json(newProduct);
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao criar produto' });
+  }
+};
+
+exports.updateProduct = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const { name, price, description, stock, category, gender, imageUrl, imagePath } = req.body;
+    const updatedProduct = await prisma.products.update({
+      where: {
+        id: parseInt(id),
+      },
+      data: {
+        name,
+        price,
+        description,
+        stock,
+        category,
+        gender,
+        imageUrl,
+        imagePath,
+      },
+    });
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao atualizar o produto!' });
+  }
+};
--- backend/controllers/UsersController.js
+++ backend/controllers/UsersController.js
@@ -2,4 +2,69 @@
 const { PrismaClient } = require('../generated/prisma');
 const prisma = new PrismaClient();
 
-// Gerenciamento de rotas vem aqui abaixo...
+// Gerenciamento de rotas
+exports.deleteUser = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const user = await prisma.users.delete({
+      where: {
+        id: parseInt(id)
+      },
+    });
+
+    res.status(200).send('Usuário deletado com sucesso!');
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao deletar o usuário!' });
+  }
+};
+
+exports.updateUser = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const {name, email, phone, password} = req.body;
+
+    const user = await prisma.users.update({
+      where: {
+        id: parseInt(id)
+      },
+      data: {
+        name,
+        email,
+        phone,
+        password
+      }
+    });
+
+    res.status(200).send('Usuário atualizado com sucesso!');
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao deletar o usuário!' });
+  }
+};
+
+exports.listUsers = async (req, res) => {
+  try {
+    const users = await prisma.users.findMany();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao buscar os usuários' });
+  }
+};
+
+exports.createUser = async (req, res) => {
+  try {
+    const { name, email, phone, password } = req.body;
+    const newUser = await prisma.users.create({
+      data: {
+        name,
+        email,
+        phone,
+        password,
+      },
+    });
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(500).json({ error: 'Erro ao criar o usuário!' });
+  }
+};
