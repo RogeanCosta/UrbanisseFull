9a387cbf2cc9d28d2c7a2c307676c9a5352db6e5
unknown <bieel180360@gmail.com>
2025-09-03 00:45:51 -0300

    rotas de deleção e atualização de usuarios

--- backend/routes/UsersRoutes.js
+++ backend/routes/UsersRoutes.js
@@ -4,8 +4,9 @@ const router = express.Router();
 const UsersController = require('../controllers/UsersController');
 
 // Definição de rotas
-// router.get('/', UsersController.list); // nome da função a ser alterada depois.
+// router.get('/', UsersController.list); 
 // TODO: DEFINIR ROTAS RESTANTE.
-
+router.delete('/users/:id', UsersController.deleteUser); // nome da função a ser alterada depois.
+router.put('/users/:id', UsersController.updateUser);
 // Exportação das rotas definidas
 module.exports = router;
