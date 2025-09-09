3da396f6f7c05fe6fcde80bc1c2cf422ef0be108
Rogean Costa <rogean.ifce@gmail.com>
2025-09-08 22:13:28 -0300

    Atualização pós merge com branch do Valnicio

--- backend/routes/UsersRoutes.js
+++ backend/routes/UsersRoutes.js
@@ -1,20 +1,14 @@
 // Configuração Básica
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const UsersController = require('../controllers/UsersController');
+const UsersController = require("../controllers/UsersController");
 
 // Definição de rotas
-<<<<<<< HEAD
-// router.get('/', UsersController.list); 
-// TODO: DEFINIR ROTAS RESTANTE.
-router.delete('/users/:id', UsersController.deleteUser); // nome da função a ser alterada depois.
-router.put('/users/:id', UsersController.updateUser);
-=======
-router.get('/users', UsersController.listUsers);
-router.post('/users', UsersController.createUser);
-router.delete('/users/:id', UsersController.deleteUser);
-router.put('/users/:id', UsersController.updateUser);
+router.delete("/users/:id", UsersController.deleteUser); // nome da função a ser alterada depois.
+router.put("/users/:id", UsersController.updateUser);
+router.get("/users", UsersController.listUsers);
+router.post("/users", UsersController.createUser);
+router.delete("/users/:id", UsersController.deleteUser);
+router.put("/users/:id", UsersController.updateUser);
 
->>>>>>> valnicio
-// Exportação das rotas definidas
 module.exports = router;
