ebb168314ccf411ff7e40308bfe2c12f8bb8d746
Rogean Costa <rogean.ifce@gmail.com>
2025-09-08 22:43:13 -0300

    [FIX] Correção de tratamento BigInt pelo express no app.js, remoção de duplicata em UsersRoutes.js

--- backend/routes/UsersRoutes.js
+++ backend/routes/UsersRoutes.js
@@ -4,8 +4,6 @@ const router = express.Router();
 const UsersController = require("../controllers/UsersController");
 
 // Definição de rotas
-router.delete("/users/:id", UsersController.deleteUser); // nome da função a ser alterada depois.
-router.put("/users/:id", UsersController.updateUser);
 router.get("/users", UsersController.listUsers);
 router.post("/users", UsersController.createUser);
 router.delete("/users/:id", UsersController.deleteUser);
