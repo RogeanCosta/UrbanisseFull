1facd3ea45de9ef76fc7240189b47e49097e4d6f
eholuizao <bieel180360@gmail.com>
2025-09-17 09:16:27 -0300

    Adição das rotas GET de intimas, calcados, stock e gender

--- backend/routes/ProductsRoutes.js
+++ backend/routes/ProductsRoutes.js
@@ -5,16 +5,26 @@ const multer = require('multer');
 const upload = multer({ storage: multer.memoryStorage() });
 const productsController = require('../controllers/ProductsController');
 
-// Definição de rotas
+// ROTAS GET
 router.get('/produtos', productsController.getProducts);
-router.get('/produtos/:id', productsController.getProduct);
 router.get('/produtos/camisas', productsController.getProductsCamisa);
 router.get('/produtos/calcas', productsController.getProductsCalca);
 router.get('/produtos/acessorios', productsController.getProductsAcessorio);
-router.delete('/produtos/:id', productsController.deleteProduct); // nome da função pode ser alterada depois.
 
-// Novas rotas
+router.get('/produtos/intimas', productsController.getProductsIntimas);
+router.get('/produtos/calcados', productsController.getProductsCalcados);
+router.get('/produtos/stock/:min', productsController.getProductsByStock);
+router.get('/produtos/gender/:gender', productsController.getProductsByGender);
+
+router.get('/produtos/:id', productsController.getProduct);
+
+// ROTAS DELETE
+router.delete('/produtos/:id', productsController.deleteProduct);
+
+// ROTAS POST
 router.post("/produtos", upload.single("file"), productsController.createProduct);
+
+// ROTAS PUT
 router.put("/produtos/:id", productsController.updateProduct);
 
 
