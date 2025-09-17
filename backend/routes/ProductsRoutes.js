60b03b829960a821bbf53164b546bfa8adc4673d
Rogean Costa <rogean.ifce@gmail.com>
2025-09-17 20:58:18 -0300

    Correção de erro por conta de rota dinâmica vindo antes de rotas fixas em ProductsRoutes.js

--- backend/routes/ProductsRoutes.js
+++ backend/routes/ProductsRoutes.js
@@ -7,16 +7,19 @@ const productsController = require('../controllers/ProductsController');
 
 // Definição de rotas
 router.get('/produtos', productsController.getProducts);
-router.get('/produtos/:id', productsController.getProduct);
 router.get('/produtos/camisas', productsController.getProductsCamisa);
 router.get('/produtos/calcas', productsController.getProductsCalca);
 router.get('/produtos/acessorios', productsController.getProductsAcessorio);
+router.get('/produtos/:id', productsController.getProduct);
 router.delete('/produtos/:id', productsController.deleteProduct); // nome da função pode ser alterada depois.
 
 // Novas rotas
-router.post("/produtos", upload.single("file"), productsController.createProduct);
-router.put("/produtos/:id", productsController.updateProduct);
-
+router.post(
+  '/produtos',
+  upload.single('file'),
+  productsController.createProduct,
+);
+router.put('/produtos/:id', productsController.updateProduct);
 
 // exportação das rotas definidas
 module.exports = router;
